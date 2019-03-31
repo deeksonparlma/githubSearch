4185acff0069ce281f9f34a75640c5afdd24696d
Dickson <dicksonparlma@gmail.com>
2019-03-31 09:18:14 +0300

    added ab API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  access_token: "b3d063c88abe89b0f32bfa7011061f1c805d7458"
+  apiUrl:'https://api.github.com/users/this.username?access_token=c2169410b588fc76cd1d4bd91c1e5374299af510'
 };
