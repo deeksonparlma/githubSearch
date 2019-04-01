ebfc24dc5d9ced8172a9d9cfe9af73312b660e33
Dickson <dicksonparlma@gmail.com>
2019-04-01 18:04:25 +0300

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'https://api.github.com/users/this.username?access_token=c2169410b588fc76cd1d4bd91c1e5374299af510'
+  apiUrl:'https://api.github.com/users/this.username?access_token=c2169410b588fc76cd1d4bd91c1e5374299af510',
+  token:"?access_token=e3891152df2a12ec5ed0b40e6205b49fef981ed9"
 };
