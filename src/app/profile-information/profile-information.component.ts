1a2988a0cc40416370c96728151ed6f38ebeb2d9
Dickson <dicksonparlma@gmail.com>
2019-04-01 15:15:40 +0300

    imported profile service

--- src/app/profile-information/profile-information.component.ts
+++ src/app/profile-information/profile-information.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { Repository } from '../repository';
+import { ProfileService } from '../profile.service';
+
 
 @Component({
   selector: 'app-profile-information',
   templateUrl: './profile-information.component.html',
-  styleUrls: ['./profile-information.component.css']
+  styleUrls: ['./profile-information.component.css'],
+  providers:[ProfileService]
 })
 export class ProfileInformationComponent implements OnInit {
 
